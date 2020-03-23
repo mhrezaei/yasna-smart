84571c4385d8f8424fbcc04d8f4e2c8ad50377f9
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-03-24 00:53:01 +0430

    First setup for arduino ESP8266

---
+++ ESP8266/yasna-smart/yasna-smart.ino
@@ -0,0 +1,182 @@
+/*********
+  Author: Mohmammad Hadi Rezaei
+  Email: hadi@yasna.team
+  Website: https://rabbitfox.com
+  Project: Yasna Smarty
+  Description: The home smart controller with ESP8266 
+*********/
+
+#include <ESP8266WiFi.h>
+//#include <DNSServer.h>
+//#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager
+
+#include <ESP8266HTTPClient.h>
+#include <Arduino_JSON.h>
+
+// RGB LED pin config
+int blueLed = 13;
+int redLed = 15;
+int greenLed = 12;
+
+// Your server IP or Domain name address
+const char* serverAddress = "http://81.12.73.170:31080/ep/esp-outputs-action.php?action=outputs_state&board=1";
+
+// Update period time set to fetch data. default: 5 seconds
+const long fetchPeriod = 10000;
+unsigned long previousRound = 0;
+String moduleState;
+
+// Dispaly an alert with RGB LED
+void ledAlert(int ledPin)
+{
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(50);
+}
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  // set RGB pin mode
+  pinMode(blueLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+ 
+  // WiFiManager
+  WiFiManager wifiManager;
+  
+  // Uncomment and run it once, if you want to erase all the stored information
+  //wifiManager.resetSettings();
+  
+  // set custom ip for portal
+  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  // First Setup open SSID name
+  wifiManager.autoConnect("Yasna Smarting");
+  
+  // if you get here you have connected to the WiFi
+  Serial.println("Connected.");
+  
+  // LED alert
+  //ledAlert(blueLed);
+}
+
+void loop(){
+  // fetch the new states
+  fetchStates();
+}
+
+// Fetching the real states
+void fetchStates(){
+  unsigned long nowRunning = millis();
+  
+  if(nowRunning - previousRound >= fetchPeriod) {
+     // Check WiFi connection status
+    //if ((WiFiMulti.run() == WL_CONNECTED)) {
+    if (1 == 1) {
+
+      // fetch data from server
+      moduleState = hgmr(serverAddress);
+
+      Serial.println(serverAddress);
+      //Serial.println(WiFi.status());
+
+      // Parse the recieved json data from server
+      JSONVar takenState = JSON.parse(moduleState);
+  
+      // Check the type of json object
+      if (JSON.typeof(takenState) == "undefined") {
+        
+        // The recieved data not be valid
+        Serial.println("The recieved data not be valid");
+
+        // LED alert
+        ledAlert(redLed);
+
+        return;
+      }
+    
+      Serial.print("JSON object = ");
+      Serial.println(takenState);
+    
+      // Find the pins number
+      JSONVar pins = takenState.keys();
+    
+      for (int i = 0; i < pins.length(); i++) {
+
+        // New pin state
+        JSONVar value = takenState[pins[i]];
+        
+        // Set the Free GPIO pins
+        pinMode(atoi(pins[i]), OUTPUT);
+
+        // Write the new state to pin
+        //digitalWrite(atoi(pins[i]), atoi(value));
+
+        Serial.print("GPIO: ");
+        Serial.print(pins[i]);
+        Serial.print(" - SET to: ");
+        Serial.println(value);
+
+      }
+
+        // LED alert
+        ledAlert(greenLed);
+
+      // Save the last fetch data from server
+      previousRound = nowRunning;
+    }
+    else {
+      // When we have any active internet connection
+      Serial.println("WiFi Disconnected");
+
+      // LED alert
+      ledAlert(redLed);
+      ledAlert(blueLed);
+    }
+  }
+}
+
+// HTTP GET method request
+String hgmr(const char* serverAddress) {
+  // use the wifi client package
+  WiFiClient client;
+
+  // use the HTTP cilent package
+  HTTPClient http;
+    
+  // Request address 
+  http.begin(client, serverAddress);
+  
+  // Respons code from server
+  int responseCode = http.GET();
+
+  Serial.println(responseCode);
+  
+  // create the data string
+  String data = "{}"; 
+  
+  if (responseCode == 200) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(responseCode);
+    data = http.getString();
+  }
+  else {
+    // Bad request or respons code
+    Serial.print("Error code: ");
+    Serial.println(responseCode);
+
+    // LED alert
+      ledAlert(redLed);
+      ledAlert(greenLed);
+  }
+
+  // Free resources one ESP8266
+  http.end();
+
+  Serial.println(data);
+  return data;
+}
