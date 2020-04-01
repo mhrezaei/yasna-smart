aa1eb2f17453c5418eb48fe5e1fbdd4891f6bae6
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-04-02 02:53:40 +0430

    Create Device Info Component

---
+++ frontend-vuejs/src/views/components/DevicesInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col-lg-4 order-lg-1">
+        <div class="card-profile-stats d-flex justify-content-center">
+            <div>
+                <span class="heading">
+                    <badger :type="statusColor" :condition="true" :label="status"></badger>
+                </span>
+                <span class="description">Status</span>
+            </div>
+            <div>
+                <span class="heading">
+                    <badger :type="'info'" :condition="true" :label="date"></badger>
+                </span>
+                <span class="description">Update</span>
+            </div>
+            <div>
+                <span class="heading">
+                    <badger :type="'info'" :condition="true" :label="nodes.toString()"></badger>
+                </span>
+                <span class="description">Nodes</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import badger from "./Badger";
+
+    export default {
+        name: "DevicesInfo",
+        components: {
+            badger,
+        },
+        props: [
+            'statusColor',
+            'status',
+            'date',
+            'nodes'
+        ],
+    }
+</script>
