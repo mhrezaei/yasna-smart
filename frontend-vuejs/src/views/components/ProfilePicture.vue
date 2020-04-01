29aaccd735c4b75c4b391e2152232de3a61d2417
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-04-02 02:53:52 +0430

    Create Profile Picture Component

---
+++ frontend-vuejs/src/views/components/ProfilePicture.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-lg-3 order-lg-2">
+        <div class="card-profile-image">
+            <img v-lazy="avatar">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProfilePicture",
+        data() {
+            return {
+                avatar: 'img/theme/avatar.png'
+            }
+        }
+    }
+</script>
