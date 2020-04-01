c0450c6006b2d667d21e9b0f1cf3e336afd750ba
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-04-02 02:53:27 +0430

    Create Cover Component

---
+++ frontend-vuejs/src/views/components/CoverSection.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="section-profile-cover section-shaped my-0">
+        <div class="shape shape-style-1 shape-primary shape-skew alpha-4"></div>
+    </section>
+</template>
