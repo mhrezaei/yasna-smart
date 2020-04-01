99c2a6611af28bad4766c8132ed01dac0af88c27
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-04-02 02:53:09 +0430

    Create Badger Component

---
+++ frontend-vuejs/src/views/components/Badger.vue
@@ -0,0 +1,19 @@
+<template>
+    <badge pill :type="type" v-if="condition">{{ label }}</badge>
+</template>
+
+<script>
+    import badge from '../../components/Badge';
+
+    export default {
+        name: "Badge",
+        components: {
+            badge
+        },
+        props: {
+            type: String,
+            condition: Boolean,
+            label: String,
+        },
+    }
+</script>
