8fd030d96347feae93ffc5b3139bb85a15b83536
Mohammad Hadi Rezaei <mr.mhrezaei@gmail.com>
2020-04-03 04:05:07 +0430

    Api modify device seeder

--- backend/database/seeds/DeviceSeeder.php
+++ backend/database/seeds/DeviceSeeder.php
@@ -15,97 +15,105 @@ class DeviceSeeder extends Seeder
         $nodes = \App\Node::all();
         foreach ($nodes as $node)
         {
-            $pinA = Str::random(6);
-            $pinB = Str::random(6);
-            $pinC = Str::random(6);
-            $x = [
-                'keys' => [
-                    $pinA => [
-                        'title' => 'Pomp Power',
-                        'pin' => '13',
-                        'type' => 'output',
-                        'status' => '0',
-                    ],
-                    $pinB => [
-                        'title' => 'Low Motor Power',
-                        'pin' => '14',
-                        'type' => 'output',
-                        'status' => '0',
-                    ],
-                    $pinC => [
-                        'title' => 'High Motor Power',
-                        'pin' => '15',
-                        'type' => 'output',
-                        'status' => '0',
-                    ],
-                ],
-                'rules' => [
-                    'power-on' => [
+            if ($node->title != "Manager")
+            {
+                $pinA = Str::random(6);
+                $pinB = Str::random(6);
+                $pinC = Str::random(6);
+                $x = [
+                    'keys' => [
                         $pinA => [
-                            'status' => '1',
-                            'when' => 'now',
+                            'title' => 'Pomp Power',
+                            'pin' => '13',
+                            'type' => 'output',
+                            'status' => '0',
+                            'allow_change' => true,
                         ],
                         $pinB => [
-                            'status' => '1',
-                            'when' => 10,
+                            'title' => 'Low Motor Power',
+                            'pin' => '14',
+                            'type' => 'output',
+                            'status' => '0',
+                            'allow_change' => true,
                         ],
                         $pinC => [
+                            'title' => 'High Motor Power',
+                            'pin' => '15',
+                            'type' => 'output',
                             'status' => '0',
-                            'when' => 'now'
+                            'allow_change' => true,
                         ],
                     ],
-                    'power-off' => [
-                        $pinA => [
-                            'status' => '0',
-                            'when' => 'now',
+                    'rules' => [
+                        'power-on' => [
+                            $pinA => [
+                                'status' => '1',
+                                'when' => 'now',
+                            ],
+                            $pinB => [
+                                'status' => '1',
+                                'when' => 10,
+                            ],
+                            $pinC => [
+                                'status' => '0',
+                                'when' => 'now'
+                            ],
                         ],
-                        $pinB => [
-                            'status' => '0',
-                            'when' => 'now',
+                        'power-off' => [
+                            $pinA => [
+                                'status' => '0',
+                                'when' => 'now',
+                            ],
+                            $pinB => [
+                                'status' => '0',
+                                'when' => 'now',
+                            ],
+                            $pinC => [
+                                'status' => '0',
+                                'when' => 'now'
+                            ],
                         ],
-                        $pinC => [
-                            'status' => '0',
-                            'when' => 'now'
+                        'speed-high' => [
+                            $pinA => [
+                                'status' => '1',
+                                'when' => 'now',
+                            ],
+                            $pinB => [
+                                'status' => '0',
+                                'when' => 'now',
+                            ],
+                            $pinC => [
+                                'status' => '1',
+                                'when' => 'now'
+                            ],
+                        ],
+                        'speed-low' => [
+                            $pinA => [
+                                'status' => '1',
+                                'when' => 'now',
+                            ],
+                            $pinB => [
+                                'status' => '1',
+                                'when' => 'now',
+                            ],
+                            $pinC => [
+                                'status' => '0',
+                                'when' => 'now'
+                            ],
                         ],
                     ],
-                    'speed-high' => [
-                        $pinA => [
-                            'status' => '1',
-                            'when' => 'now',
-                        ],
-                        $pinB => [
-                            'status' => '0',
-                            'when' => 'now',
-                        ],
-                        $pinC => [
-                            'status' => '1',
-                            'when' => 'now'
-                        ],
-                    ],
-                    'speed-low' => [
-                        $pinA => [
-                            'status' => '1',
-                            'when' => 'now',
-                        ],
-                        $pinB => [
-                            'status' => '1',
-                            'when' => 'now',
-                        ],
-                        $pinC => [
-                            'status' => '0',
-                            'when' => 'now'
-                        ],
-                    ],
-                ],
-            ];
+                    'current_rule' => false,
+                    'allow_change' => true,
+                ];
 
-            $data = [
-                'node_id' => $node->id,
-                'title' => $node->title,
-                'icon' => 'ni-ui-04',
-                'key_status' => json_encode($x),
-            ];
-            \App\Device::create($data);
+                $data = [
+                    'node_id' => $node->id,
+                    'title' => $node->title,
+                    'icon' => 'ni-ui-04',
+                    'key_status' => json_encode($x),
+                ];
+                \App\Device::create($data);
+            }
         }
     }
 }
